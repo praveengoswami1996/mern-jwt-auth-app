b4bf35b10cb5a1cd0f9f2309b9bfef9fa767434b
Praveen Goswami <praveengoswami@Praveens-MacBook-Air.local>
2025-07-11 20:41:28 +0530

    chore(ui): setup Chakra UI for component styling and theming

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { Button } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom"
+
+const Home = () => {
+  return <div>
+    <Button>Click me!</Button>
+  </div>
+}
 
 function App() {
-
   return (
-    <>
-      Test
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./config/queryClient.ts";
+import { Provider } from "./components/ui/provider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools position="bottom" initialIsOpen={false} />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
+  </StrictMode>
+);
